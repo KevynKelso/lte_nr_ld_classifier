1f8eb1e9f80ba53d7ccc14565b9e108335e2c7b5
Kevyn Kelso <kevynkelso@outlook.com>
2025-05-09 18:57:51 -0600

    fix: update constants

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
 # Deep learning params
 DATA_DIR = "data/lr_training_data"  # Folder with subfolders per class
 IMG_SIZE = (128, 128)  # Match spectrogram dimensions
-BATCH_SIZE = 256
-EPOCHS = 50
+BATCH_SIZE = 128
+EPOCHS = 100
 TEST_SPLIT = 0.2  # 20% for testing
 VAL_SPLIT = 0.2  # 20% of training for validation
 
